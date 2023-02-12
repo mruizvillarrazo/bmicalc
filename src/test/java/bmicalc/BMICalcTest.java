1d0bae180ee77aae4f282bf79327711cf59c7fdc
mruiz <mruizvillarrazo@uma.es>
2023-02-12 13:26:21 +0100

    Elaboracion de algunos tests

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -1,15 +1,50 @@
 package bmicalc;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BMICalcTest {
 	
+	private final BMICalcImpl calculadora = new BMICalcImpl();	
+	
 	@Test
 	@DisplayName("Example Test Method.")
 	public void bmi() {
 		assertEquals(true, true);
 	}
+	
+	
+	@Test
+	@DisplayName("En el IBM intentar dividir una masa entre 0")
+    void division() {
+		Exception exception = assertThrows(ArithmeticException.class, () ->
+        calculadora.bmi(2, 0));
+        assertEquals("/ by zero", exception.getMessage());
+    }
+	
+	@Test
+	@DisplayName("Comprobar intervalos 1")
+    void intervalos1() {
+		assertEquals("UNDERWEIGHT", calculadora.category(5));
+    }
+	
+	@Test
+	@DisplayName("Comprobar intervalos 2")
+    void intervalos2() {
+		assertEquals("NORMAL", calculadora.category(21));
+    }
+	
+	@Test
+	@DisplayName("Comprobar intervalos 3")
+    void intervalos3() {
+		assertEquals("OVERWEIGHT", calculadora.category(27));
+    }
+	
+	
+	
 }
