39ac80c31b4a26fde38c2399315f3aafebf7f1ef
mruiz <mruizvillarrazo@uma.es>
2023-02-12 12:33:02 +0100

    Implementacion de los metodos

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,15 +3,30 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		return 0.0;
+		double result = (mass)/(Math.pow(height,2));
+		return result;
 	}
 
 	public String category(double bmi) {
-		return "";
+		String cat="";
+		if(bmi<18.5) {
+			cat="UNDERWEIGHT";
+		}else if (bmi>18.5 && bmi<24.9) {
+			cat="NORMAL";
+		}else if(bmi>25.0 && bmi<29.9) {
+			cat="OVERWEIGHT";
+		}else {
+			cat="OBSESE";
+		}
+		return cat;
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return false;
+		boolean estar=false;
+		if((gender=='M' && waistCircumference>90)||(gender=='F' && waistCircumference>80)){
+			estar=true;
+		}
+		return estar;
 	}
 
 }
