c4d37c672194485d10b7ab050a40e84f672b7cbd
mruiz <mruizvillarrazo@uma.es>
2023-03-24 19:24:09 +0100

    Finalizacion de la interfaz

--- src/main/java/gui/BMIControlador.java
+++ src/main/java/gui/BMIControlador.java
@@ -25,26 +25,53 @@ public class BMIControlador implements ActionListener {
 	        double cintura = vista.getCintura();
 	        char genero = vista.getGenero();
 	        bmi=new BMICalcImpl();
-	        double bmi_calc = bmi.bmi(peso, altura);
+	        
+	        
+	        double bmi_calc ;
 
-	        String category_calc = bmi.category(bmi_calc);
+	        String category_calc = null;
 
-	        boolean obesity_calc = bmi.abdominalObesity(cintura,genero);
+	        boolean obesity_calc = false;
+	        
 	        String obesity_text= "";
-	        if(obesity_calc==true) {
-	        	obesity_text="True";
-	        }else {
-	        	obesity_text="False";
-	        }
+	        
+	       
+	        
 	        
 	        if (altura!=0 && peso!=0 && cintura!=0) {
+	        	
+	        	 
+	        	
+	        	bmi_calc = bmi.bmi(peso, altura);
+	        	category_calc = bmi.category(bmi_calc);
+	        	if((category_calc=="OBESE" && genero=='M' && cintura<90) || (category_calc=="OBESE" && genero=='F' && cintura<80)) {
+	        		throw new RuntimeException("No tienen sentido estos valores");
+	        	}
+	        	obesity_calc = bmi.abdominalObesity(cintura,genero);
+	        	if(obesity_calc==true) {
+		        	obesity_text="¡Hora de hacer deporte!";
+		        }else {
+		        	obesity_text="¡Estas en forma!";
+		        }
+	        	
 	            vista.setBMI(Double.toString(bmi_calc));
 	            vista.setCategory(category_calc);
+	            
 	            vista.setObesity(obesity_text);
+	            
+	            
 	        }else if(cintura==0) {
+	        	bmi_calc = bmi.bmi(peso, altura);
+	        	category_calc = bmi.category(bmi_calc);
 	        	vista.setBMI(Double.toString(bmi_calc));
 	            vista.setCategory(category_calc);
-	        }else {
+	        }else if(cintura!=0 && altura==0 && peso==0){
+	        	obesity_calc = bmi.abdominalObesity(cintura,genero);
+	        	if(obesity_calc==true) {
+		        	obesity_text="¡Hora de hacer deporte!";
+		        }else {
+		        	obesity_text="¡Estas en forma!";
+		        }
 	            vista.setObesity(obesity_text);
 
 	        }
--- src/main/java/gui/BMIVista.java
+++ src/main/java/gui/BMIVista.java
@@ -33,7 +33,7 @@ public class BMIVista extends JFrame {
 	
 	private JButton bCalcular, bExit;
 	private ButtonGroup grupobotones;
-	private JRadioButton rb_Female;
+	private JRadioButton rb_Female,rb_Male;
 
 	private JSpinner spinner_masa, spinner_altura, spinner_cintura;
 	private JLabel output_BMI,output_obesity,category_output;
@@ -57,7 +57,7 @@ public class BMIVista extends JFrame {
 	 */
 	private void initialize() {
 		//frame = new JFrame();
-		getContentPane().setBackground(new Color(143, 188, 143));
+		getContentPane().setBackground(new Color(240, 230, 140));
 		getContentPane().setForeground(new Color(100, 149, 237));
 		setBounds(100, 100, 536, 519);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -68,7 +68,7 @@ public class BMIVista extends JFrame {
 		panel_masa.setBounds(134, 63, 58, 35);
 		getContentPane().add(panel_masa);
 		
-		JSpinner spinner_masa = new JSpinner();
+		spinner_masa = new JSpinner();
 		spinner_masa.setModel(new SpinnerNumberModel(0.0, 0.0, 350.0, 1.0));
 		panel_masa.add(spinner_masa);
 		
@@ -82,7 +82,7 @@ public class BMIVista extends JFrame {
 		panel_altura.setBounds(134, 139, 58, 35);
 		getContentPane().add(panel_altura);
 		
-		JSpinner spinner_altura = new JSpinner();
+		spinner_altura = new JSpinner();
 		spinner_altura.setModel(new SpinnerNumberModel(0.0, null, 250.0, 1.0));
 		panel_altura.add(spinner_altura);
 		
@@ -96,8 +96,8 @@ public class BMIVista extends JFrame {
 		panel_cintura.setBounds(134, 210, 58, 35);
 		getContentPane().add(panel_cintura);
 		
-		JSpinner spinner_cintura = new JSpinner();
-		spinner_cintura.setModel(new SpinnerNumberModel(0, 0, 150, 1));
+		spinner_cintura = new JSpinner();
+		spinner_cintura.setModel(new SpinnerNumberModel(0.0, 0.0, 150.0, 1.0));
 		panel_cintura.add(spinner_cintura);
 		
 		JLabel Label_cintura = new JLabel("Cintura (cm)");
@@ -105,23 +105,24 @@ public class BMIVista extends JFrame {
 		Label_cintura.setBounds(27, 199, 97, 61);
 		getContentPane().add(Label_cintura);
 		
-		JRadioButton rb_Male = new JRadioButton("M");
+		rb_Male = new JRadioButton("M");
+		rb_Male.setSelected(true);
 		rb_Male.setBackground(new Color(112, 128, 144));
 		rb_Male.setFont(new Font("Century Gothic", Font.PLAIN, 14));
-		rb_Male.setSelected(true);
 		rb_Male.setBounds(134, 276, 58, 21);
 		getContentPane().add(rb_Male);
 		
-		grupobotones.add(rb_Male);
 		
-		JRadioButton rb_Female = new JRadioButton("F");
+		
+		rb_Female = new JRadioButton("F");
 		rb_Female.setBackground(new Color(112, 128, 144));
 		rb_Female.setFont(new Font("Century Gothic", Font.PLAIN, 14));
 		rb_Female.setBounds(134, 322, 58, 21);
 		getContentPane().add(rb_Female);
 		
+		grupobotones = new ButtonGroup();
 		grupobotones.add(rb_Female);
-		
+		grupobotones.add(rb_Male);
 			
 		
 		
@@ -132,30 +133,30 @@ public class BMIVista extends JFrame {
 		getContentPane().add(Label_genero);
 		
 		bCalcular = new JButton("Calcular ");
-		bCalcular.setBackground(new Color(0, 0, 0));
-		bCalcular.setForeground(new Color(147, 112, 219));
+		bCalcular.setBackground(new Color(255, 255, 255));
+		bCalcular.setForeground(new Color(0, 0, 0));
 		bCalcular.setFont(new Font("Century Gothic", Font.PLAIN, 14));
 		bCalcular.setBounds(49, 420, 143, 44);
 		getContentPane().add(bCalcular);
 		
 		JPanel panel_inputs = new JPanel();
-		panel_inputs.setBackground(new Color(173, 216, 230));
+		panel_inputs.setBackground(new Color(211, 211, 211));
 		panel_inputs.setBorder(new MatteBorder(5, 5, 5, 5, (Color) new Color(0, 0, 0)));
 		panel_inputs.setForeground(UIManager.getColor("menu"));
 		panel_inputs.setBounds(10, 32, 207, 362);
 		getContentPane().add(panel_inputs);
 		panel_inputs.setLayout(new CardLayout(0, 0));
 		
-		JLabel output_BMI = new JLabel("");
+		output_BMI = new JLabel("");
 		output_BMI.setEnabled(false);
 		output_BMI.setToolTipText("");
-		output_BMI.setForeground(new Color(255, 218, 185));
-		output_BMI.setFont(new Font("Century Gothic", Font.PLAIN, 10));
-		output_BMI.setBounds(339, 63, 169, 44);
+		output_BMI.setForeground(new Color(0, 0, 0));
+		output_BMI.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		output_BMI.setBounds(339, 54, 169, 44);
 		getContentPane().add(output_BMI);
 		
-		JButton bExit = new JButton("Salir");
-		bExit.setBackground(new Color(0, 0, 0));
+		bExit = new JButton("Salir");
+		bExit.setBackground(new Color(255, 255, 255));
 		bExit.setFont(new Font("Century Gothic", Font.PLAIN, 14));
 		bExit.setBounds(331, 420, 143, 44);
 		getContentPane().add(bExit);
@@ -165,28 +166,28 @@ public class BMIVista extends JFrame {
 		Label_BMI.setBounds(254, 49, 75, 61);
 		getContentPane().add(Label_BMI);
 		
-		JLabel Label_Category = new JLabel("Category");
-		Label_Category.setFont(new Font("Century Gothic", Font.PLAIN, 14));
-		Label_Category.setBounds(254, 149, 75, 61);
-		getContentPane().add(Label_Category);
+		JLabel lblCategoria = new JLabel("Categoria");
+		lblCategoria.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		lblCategoria.setBounds(254, 149, 75, 61);
+		getContentPane().add(lblCategoria);
 		
-		JLabel ouput_obesity = new JLabel("");
-		ouput_obesity.setForeground(new Color(255, 218, 185));
-		ouput_obesity.setFont(new Font("Century Gothic", Font.PLAIN, 10));
-		ouput_obesity.setEnabled(false);
-		ouput_obesity.setBounds(339, 276, 169, 44);
-		getContentPane().add(ouput_obesity);
+		output_obesity = new JLabel("");
+		output_obesity.setForeground(new Color(0, 0, 0));
+		output_obesity.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		output_obesity.setEnabled(false);
+		output_obesity.setBounds(331, 256, 169, 44);
+		getContentPane().add(output_obesity);
 		
-		JLabel Label_Obesity = new JLabel("Obesity");
-		Label_Obesity.setFont(new Font("Century Gothic", Font.PLAIN, 14));
-		Label_Obesity.setBounds(254, 256, 75, 61);
-		getContentPane().add(Label_Obesity);
+		JLabel lblObesidad = new JLabel("Obesidad");
+		lblObesidad.setFont(new Font("Century Gothic", Font.PLAIN, 14));
+		lblObesidad.setBounds(254, 256, 75, 61);
+		getContentPane().add(lblObesidad);
 		
-		JLabel category_output = new JLabel("");
-		category_output.setForeground(new Color(255, 218, 185));
-		category_output.setFont(new Font("Century Gothic", Font.PLAIN, 10));
+		category_output = new JLabel("");
+		category_output.setForeground(new Color(0, 0, 0));
+		category_output.setFont(new Font("Century Gothic", Font.PLAIN, 14));
 		category_output.setEnabled(false);
-		category_output.setBounds(339, 166, 169, 44);
+		category_output.setBounds(339, 149, 169, 44);
 		getContentPane().add(category_output);
 		
 		
@@ -205,7 +206,7 @@ public class BMIVista extends JFrame {
 	
 	
 	public double getMasa() {
-	    return ((Double)spinner_masa.getValue()*0.01);
+	    return ((Double)spinner_masa.getValue());
 
 	}
 	
@@ -215,7 +216,7 @@ public class BMIVista extends JFrame {
 	}
 	
 	public double getCintura() {
-	    return ((double)spinner_cintura.getValue());
+	    return ((Double)spinner_cintura.getValue());
 	   
 
 	}
